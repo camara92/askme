fc3fd4ce03f371c2131ad9d57e4995e0ad81a851
CAMARA <88945535+camara92@users.noreply.github.com>
2023-12-06 02:29:44 +0100

    formulaire pour poser une question dans le projet askme

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Form\QuestionType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class QuestionController extends AbstractController
 {
     #[Route('/question/ask', name: 'app_question_form')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $formQuestion = $this->createForm(QuestionType::class); 
+        $formQuestion->handleRequest($request); 
+        if($formQuestion->isSubmitted() && $formQuestion->isValid()){
+            dd($formQuestion->getData()); 
+        }
         return $this->render('question/index.html.twig', [
-            'controller_name' => 'QuestionController',
+            'form' => $formQuestion->createView()
         ]);
     }
 }
