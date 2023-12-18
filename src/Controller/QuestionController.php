7070a7576fb063600366df087e787cdad3643a42
CAMARA <88945535+camara92@users.noreply.github.com>
2023-12-18 03:58:39 +0100

    processus de commentaire

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Question;
+use App\Form\CommentType;
 use App\Form\QuestionType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,35 +15,53 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 #[Route('/question', name: 'question_')]
 class QuestionController extends AbstractController
 {
-  #[Route('/ask', name: 'form')]
-  public function index(Request $request, EntityManagerInterface $em): Response
-  {
+    #[Route('/ask', name: 'form')]
+    public function index(Request $request, EntityManagerInterface $em): Response
+    {
 
-    $question = new Question();
-    $formQuestion = $this->createForm(QuestionType::class, $question);
+        $question = new Question();
+        $formQuestion = $this->createForm(QuestionType::class, $question);
 
-    $formQuestion->handleRequest($request);
+        $formQuestion->handleRequest($request);
 
-    if ($formQuestion->isSubmitted() && $formQuestion->isValid()) {
-      $question->setNbrOfResponse(0);
-      $question->setRating(0);
-      $question->setCreatedAt(new \DateTimeImmutable());
-      $em->persist($question);
-      $em->flush();
-      $this->addFlash('success', 'Votre question a été ajoutée');
-      return $this->redirectToRoute('home');
+        if ($formQuestion->isSubmitted() && $formQuestion->isValid()) {
+            $question->setNbrOfResponse(0);
+            $question->setRating(0);
+            $question->setCreatedAt(new \DateTimeImmutable());
+            $em->persist($question);
+            $em->flush();
+            $this->addFlash('success', 'Votre question a été ajoutée');
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('question/index.html.twig', [
+            'form' => $formQuestion->createView(),
+        ]);
     }
 
-    return $this->render('question/index.html.twig', [
-      'form' => $formQuestion->createView(),
-    ]);
-  }
+    #[Route('/{id}', name: 'show')]
+    public function show(Question $question, Request $request, EntityManagerInterface $em): Response
+    {
+        // la réponse :
+        $comment = new Comment();
+        $commentForm = $this->createForm(CommentType::class, $comment);
 
-  #[Route('/{id}', name: 'show')]
-  public function show(Question $question): Response
-  {
-    return $this->render('question/show.html.twig', [
-      'question' => $question,
-    ]);
-  }
+        $commentForm->handleRequest($request);
+
+        if ($commentForm->isSubmitted() && $commentForm->isValid()) {
+
+            $comment->setCreatedAt(new \DateTimeImmutable());
+            $comment->setRating(0);
+            $comment->setQuestion($question); 
+            $em->persist($comment);
+            $em->flush();
+            $this->addFlash('success', 'Votre commentaire a été ajoutée');
+            return $this->redirect($request->getUri());
+        }
+
+        return $this->render('question/show.html.twig', [
+            'question'=>$question, 
+            'form' => $commentForm->createView(),
+        ]);
+    }
 }
--- src/Entity/Question.php
+++ src/Entity/Question.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\QuestionRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -31,6 +33,14 @@ class Question
 
     #[ORM\Column]
     private ?int $nbrOfResponse = null;
+
+    #[ORM\OneToMany(mappedBy: 'question', targetEntity: Comment::class, orphanRemoval: true)]
+    private Collection $comments;
+
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -93,6 +103,36 @@ class Question
     public function setNbrOfResponse(int $nbrOfResponse): static
     {
         $this->nbrOfResponse = $nbrOfResponse;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Comment>
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): static
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments->add($comment);
+            $comment->setQuestion($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): static
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getQuestion() === $this) {
+                $comment->setQuestion(null);
+            }
+        }
 
         return $this;
     }
