b62264ea73d4a1c78a819b2733e916dccdd134b5
CAMARA <88945535+camara92@users.noreply.github.com>
2023-12-19 10:58:55 +0100

    vue des questions posées par auteur + routes listes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,12 +5,30 @@ namespace App\Controller;
 use App\Entity\Question;
 use App\Repository\QuestionRepository;
 use Container0UEOsho\getQuestionService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private $entityManager; 
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager= $entityManager;
+    }
+
+     
+    #[Route("/listes", name:"listes")]
+ 
+    public function listesQuestions(): Response
+    { 
+        $listesQuestions = $this->entityManager->getRepository(Question::class)->findAll();
+
+        return $this->render('home/listsquestion.html.twig', [
+            'listesQuestions'=>$listesQuestions
+        ]); 
+    }
     #[Route('/', name: 'home')]
     public function index(QuestionRepository $questionRepository): Response
     {
