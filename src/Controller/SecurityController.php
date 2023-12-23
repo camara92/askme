66c20781f4ec4b7bb957fdd82931d742cfdbbd5c
CAMARA <88945535+camara92@users.noreply.github.com>
2023-12-23 06:43:18 +0100

    connexion des utilisateurs

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -11,6 +11,8 @@ use Symfony\Component\HttpFoundation\Response;
 
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Event\AuthenticationEvent;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
@@ -34,10 +36,18 @@ class SecurityController extends AbstractController
         ]);
     }
     #[Route('/login', name: 'login')]
-    public function login(): Response
+    public function login(AuthenticationUtils $authenticationUtils, ): Response
     {
-        return $this->render('security/index.html.twig', [
-            'controller_name' => 'connexion',
+        if($this->getUser()){
+            return $this->redirectToRoute('home'); 
+            
+        }
+        $error = $authenticationUtils->getLastAuthenticationError(); 
+        $username = $authenticationUtils->getLastUsername(); 
+
+        return $this->render('security/login.html.twig', [
+            'error' => $error,
+            'username'=>$username
         ]);
     }
     #[Route('/logout', name: 'logout')]
